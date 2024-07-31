30cda3785cd0a9e7688ae050b330e8cea3132af9
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-07-31 16:28:02 +0900

    Update 무인도여행.cpp

--- Programmers/Lv.2/무인도여행.cpp
+++ Programmers/Lv.2/무인도여행.cpp
@@ -3,54 +3,57 @@
 #include <iostream>
 using namespace std;
 
-vector<vector<int>> find(vector<vector<int>> land, int y, int x) {
-    land[y][x] = 0;
+int sum = 0;
+
+void find(vector<string>& land, int y, int x) {
+    sum += stoi(string(1, land[y][x]));
+    land[y][x] = 'X';
     int serow = land.size();
     int garow = land[0].size();
-    if (x + 1 < garow)
-    {
-        if (land[y][x + 1] == 1)
-        {
-            land = find(land, y, x + 1);
-        }
+
+    if (x + 1 < garow && land[y][x + 1] != 'X') {
+        find(land, y, x + 1);
     }
-    if (y + 1 < serow)
-    {
-        if (land[y + 1][x] == 1)
-        {
-            land = find(land, y + 1, x);
-        }
+    if (y + 1 < serow && land[y + 1][x] != 'X') {
+        find(land, y + 1, x);
     }
-    if (x - 1 >= 0)
-    {
-        if (land[y][x - 1] == 1)
-        {
-            land = find(land, y, x - 1);
-        }
+    if (x - 1 >= 0 && land[y][x - 1] != 'X') {
+        find(land, y, x - 1);
     }
-    if (y - 1 >= 0)
-    {
-        if (land[y - 1][x] == 1)
-        {
-            land = find(land, y - 1, x);
-        }
+    if (y - 1 >= 0 && land[y - 1][x] != 'X') {
+        find(land, y - 1, x);
     }
-    return land;
 }
 
 vector<int> solution(vector<string> maps) {
     vector<int> answer;
-    int x_dir[4] = { -1,1,0,0 };
-    int y_dir[4] = { 0,0,-1,1 };
-    bool visited[101][101] = { false };
+    int serow = maps.size();
+    int garow = maps[0].size();
+
+    for (int i = 0; i < serow; i++) {
+        for (int j = 0; j < garow; j++) {
+            if (maps[i][j] != 'X') {
+                sum = 0;
+                find(maps, i, j);
+                if (sum != 0) {
+                    answer.push_back(sum);
+                }
+            }
+        }
+    }
+    if (answer.empty())
+        answer.push_back(-1);
+    for (int i = 0; i < answer.size(); i++)
+    {
+        for (int j = i; j < answer.size(); j++)
+        {
+            if (answer[i] > answer[i + 1])
+            {
+                int tmp = answer[i];
+                answer[i] = answer[j];
+                answer[j] = tmp;
+            }
+        }
+    }
     return answer;
-}
-
-
-
-int main() 
-{
-    vector<string> maps = { "X591X", "X1X5X", "X231X", "1XXX1" };
-    solution(maps);
-    
 }
