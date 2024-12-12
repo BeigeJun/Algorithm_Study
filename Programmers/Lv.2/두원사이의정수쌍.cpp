f01d96d739ff458cc8a18aa41b54f31edc44ad28
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-12-12 17:44:08 +0900

    Update 두원사이의정수쌍.cpp

--- Programmers/Lv.2/두원사이의정수쌍.cpp
+++ Programmers/Lv.2/두원사이의정수쌍.cpp
@@ -1,26 +1,21 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include <string>
+#include <vector>
 #include <math.h>
 
-bool Euclidiean_Distance(int x1, int y1, int x2, int y2, int r1, int r2)
-{
-    double dbDistance = sqrt((x2 - x1) * (x2 - x1) +(y2 - y1) * (y2 - y1));
-    if (dbDistance >= r1 && dbDistance <= r2)
-        return true;
-    else
-        return false;
-}
+using namespace std;
 
 long long solution(int r1, int r2) {
-    long long answer = 0;
-    for (int i = -r2; i <= r2; i++)
-    {
-        for (int j = -r2; j <= r2; j++)
-        {
-            if(Euclidiean_Distance(i,j, 0,0,r1,r2))
-                answer+=1;
-        }
-    }
-    return answer;
+	long long answer = 0;
+
+	for (int i = 1; i <= r2; i++) {
+		int minY = ceil(sqrt((long long)r1 * r1 - (long long)i * i));
+		int maxY = floor(sqrt((long long)r2 * r2 - (long long)i * i));
+
+		if (r1 < i)
+			minY = 0;
+
+		answer += (maxY - minY + 1);
+	}
+
+	return answer * 4;
 }
